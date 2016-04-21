23bd685e5eeb87c93d8ee3f9e2e01b63f42c2106
mashybuttons <katherine.sj@gmail.com>
2016-04-21 15:31:45 -0700

    finish tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
+  newTree = _.extend(newTree, treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,10 +12,44 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree (value);
+  this.children.push(child);
 };
 
+
+
+
+
+
+
+
+
 treeMethods.contains = function(target) {
+  var result = false;
+    
+
+
+  var recurs = function (node, target) {
+    
+
+    if (node.value === target) {
+      result = true;
+    } else {
+      for (var x = 0; x < node.children.length; x++) {
+        recurs (node.children[x], target);
+      }
+    }
+  };
+
+  recurs(this, target);
+  return result;
 };
+
+
+
+
+
+
 
 
 
