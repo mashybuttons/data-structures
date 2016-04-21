fb152f4dc010ca59c176fbfc4737cc11235ea3e5
mashybuttons <katherine.sj@gmail.com>
2016-04-21 09:57:47 -0700

    finish pseudoclassical queue, fix syntax errors on stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
+  this.deletecount = 0;
+
 };
+
+Queue.prototype.size = function() {
+  if (this.length - this.deletecount < 0) {
+    return 0;
+  } else {
+    return this.length - this.deletecount;
+  }
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function() {
+  var dequeued = this.storage[this.deletecount];
+  delete this.storage[this.deletecount];
+  this.deletecount++;
+  return dequeued;
+};
+
 
 
