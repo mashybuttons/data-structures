e11c668699558d66bf51c7f2dc60af392c8b67e2
mashybuttons <katherine.sj@gmail.com>
2016-04-21 13:24:29 -0700

    complete linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,51 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    if (list.tail === null) {
+      list.tail = list.head;
+    } else {
+      var oldTail = list.tail;
+      oldTail.next = newNode;
+      list.tail = newNode;
+      if (list.head.next === null) {
+        list.head.next = list.tail;
+      } 
+    }
   };
 
   list.removeHead = function() {
+    var removedhead = list.head.value;
+    list.head = list.head.next;
+    return removedhead;
+    //change list.head to look at list.head.next
+
   };
 
   list.contains = function(target) {
+
+    var recurs = function(target, node) {
+
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return recurs(target, node.next);
+      }
+
+    };
+     
+    return recurs(target, list.head);
+
   };
+
+
+
+
 
   return list;
 };
