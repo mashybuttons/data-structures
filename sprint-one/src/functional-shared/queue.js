9ab12c2c0c208d333d8b2603b54ae158ecfbb9cd
mashybuttons <katherine.sj@gmail.com>
2016-04-21 09:37:08 -0700

    finish functional-shared for queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
+  var obj = {};
+  obj = _.extend(obj, queueMethods);
+  obj.storage = {};
+  obj.deletecount = 0;
+  obj.length = 0;
+  return obj;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    if (this.length - this.deletecount < 0) {
+      return 0; 
+    } else {
+      return this.length - this.deletecount;
+    }
+  },
+  enqueue: function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  dequeue: function() {
+    var dequeued = this.storage[this.deletecount];
+    delete this.storage[this.deletecount];
+    this.deletecount++;
+    return dequeued;
+
+  }
+
+};
 
 
