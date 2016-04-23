027e343ad3ad11595f57d75c4ff5a2e2566584fc
mashybuttons <katherine.sj@gmail.com>
2016-04-22 18:47:44 -0700

    complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  var result = false;
+  _.each(this._storage, function(value) {
+    if (value === item) {
+      result = true;
+    }
+  });
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
