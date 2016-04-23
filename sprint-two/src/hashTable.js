81fdb076c8b7b23892a7766d75be3615849a6d96
mashybuttons <katherine.sj@gmail.com>
2016-04-22 17:38:37 -0700

    finish hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,53 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  //[[k,v]]
+
+
+  if (!bucket) {
+    this._storage.set(index, [[k, v]]);
+    
+  } else {
+
+    bucket.push([k, v]);
+  }
+    
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var bucket = this._storage.get(index);
+  var result;
+
+  _.each(bucket, function(tuples) {
+    if (tuples[0] === k) {
+
+
+      result = tuples[1];
+    }
+  });
+  return result;                                                                                                 
+                                                                                                                                                                                                                  
+
+
+
+  
+
+};                             
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storageIndex = this._storage.get(index);
+  var result;
+  _.each(storageIndex, function(tuples, i) {
+    if (tuples[0] === k) {
+      result = storageIndex.splice(i, 1);
+    }
+  });
+  return result;    
+
 };
 
 
